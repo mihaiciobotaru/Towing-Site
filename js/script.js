d0f4904840d5fce05dd7d74ead679cc8bbe41fc2
Mihai Ciobotaru <mihai2096@gmail.com>
2021-04-19 09:19:15 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 const menuBtn = document.querySelector('.togglerBtn');
 const menuLinksBkg = document.querySelector('.nav ul');
 const menuLinksBtns = document.querySelector('.nav li');
+const htmlSelection = document.querySelector('html');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
 if(!menuOpen) {
   menuBtn.classList.add('open');
   menuLinksBkg.classList.add('open');
   menuLinksBtns.classList.add('open');
+  htmlSelection.style.overflow ="hidden";
   menuOpen = true;
 
 } else {
   menuBtn.classList.remove('open');
   menuLinksBkg.classList.remove('open');
   menuLinksBtns.classList.remove('open');
+  htmlSelection.style.overflow ="scroll";
   menuOpen = false;
 }
 });    
